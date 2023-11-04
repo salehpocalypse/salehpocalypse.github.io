28f740e0bd4fc631cfe298e2c5043d5ee02c8e3e
salehpocalypse <148906809+salehpocalypse@users.noreply.github.com>
2023-11-04 11:29:01 -0400

    Basic setup for code (no game yet)

---
+++ index.js
@@ -0,0 +1,22 @@
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+let keys = {};
+let panicSel = document.getElementById("panic-sel"),
+panicUrl = document.getElementById("panic");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+document.appendChild(canvas);
+
+//document.body.addEventListener("load", (e) => {});
+
+addEventListener("keydown", (e) => {
+  keys[e.key.toLowerCase()] = true;
+  if (e[`${panicSel.selectedIndex.toString().toLowerCase()}Key`] && keys[","]) {
+    let href = panicUrl.value.includes("://") ? panicUrl.value : `https://${panicUrl.value}`;
+    open(href, "_top");
+  }
+});
+addEventListener("keyup", (e) => {
+  keys[e.key.toLowerCase()] = false;
+});
